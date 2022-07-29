b1cb50f25153651992e5b278647fcf13c5d56fe3
Jamile <milli.santana2017@gmail.com>
2022-07-29 14:22:19 -0300

    feat: Exibe top 2 hits mais tocados no momento

---
+++ desafio5.sql
@@ -0,0 +1,15 @@
+
+-- SELECT 
+-- 	cancao AS cancao,
+-- 	COUNT(usuario_id) AS reproducoes
+--     FROM SpotifyClone.cancao
+-- 	JOIN historico_de_reproducao AS h ON usuario_id
+--     GROUP BY h.cancao_id
+--     ORDER BY reproducoes, ASC cancao LIMIT 2;
+SELECT 
+	cancao AS cancao,
+	COUNT(cancao) AS reproducoes
+    FROM SpotifyClone.cancao AS c
+	JOIN historico_de_reproducao AS h ON c.cancao_id = h.cancao_id
+    GROUP BY c.cancao
+    ORDER BY reproducoes DESC , cancao ASC LIMIT 2;
